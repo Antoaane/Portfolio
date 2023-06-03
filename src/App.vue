f34de219b33b07a494ea0c2586216c18d039b051
Antoaane <antoine.lauzis@univ-reims.fr>
2023-06-03 14:59:17 +0200

    menu position & colors

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <header>
       <nav class="h-full container flex justify-between items-center">
-        <a href="./home.html">
+        <RouterLink class="title" to="/">
             <span>Mon </span>
-            <span class="vps">VPS</span>
-        </a>
+            <span id="vps">VPS</span>
+        </RouterLink>
         <ul class="hidden" id="menu">
-          <li><RouterLink to="/works">WORKS</RouterLink></li>
-          <li><RouterLink to="/">PORTFOLIO</RouterLink></li>
-          <li><RouterLink to="/">MON CV</RouterLink></li>
-          <li><RouterLink to="/">LINKED IN</RouterLink></li>
-          <li><RouterLink to="/">CONTACT</RouterLink></li>
+          <li><RouterLink class="menu-item" to="/works">WORKS</RouterLink></li>
+          <li><RouterLink class="menu-item" to="/">PORTFOLIO</RouterLink></li>
+          <li><RouterLink class="menu-item" to="/">MON CV</RouterLink></li>
+          <li><RouterLink class="menu-item" to="/">LINKED IN</RouterLink></li>
+          <li><RouterLink class="menu-item" to="/">CONTACT</RouterLink></li>
         </ul>
         
         <div class="close" id="hamburger">
@@ -34,18 +34,25 @@ import { ref, onMounted } from 'vue';
 import { RouterLink, RouterView } from 'vue-router';
 
 const hamburger = ref(null);
-const menu = ref(null);
 const tomato = ref(null);
 const cheese = ref(null);
 const steak = ref(null);
 
+const menu = ref(null);
+const menuBg = ref(null);
+
+const vps = ref(null);
+
 onMounted(() => {
   const hamburger = document.getElementById('hamburger');
-  const menu = document.getElementById('menu');
-  const menuBg = document.getElementById('menu-bg');
   const tomato = document.getElementById('tomato');
   const cheese = document.getElementById('cheese');
   const steak = document.getElementById('steak');
+
+  const menu = document.getElementById('menu');
+  const menuBg = document.getElementById('menu-bg');
+
+  const vps = document.getElementById('vps');
 
   hamburger.addEventListener('click', () => {
       if (menu.classList.contains('hidden')) {
@@ -55,6 +62,8 @@ onMounted(() => {
 
           menu.classList.remove('hidden');
           menuBg.style.animation = "menuBg 0.5s cubic-bezier(0.68,-0.55,0.29,1.65) forwards";
+
+          vps.style.color = "#526C67";
       } else {
           tomato.style.animation = "tomatoBack 0.5s cubic-bezier(0.68,-0.55,0.29,1.65) forwards"
           cheese.style.transform = "scaleX(1)";
@@ -62,6 +71,8 @@ onMounted(() => {
 
           menu.classList.add('hidden');
           menuBg.style.animation = "menuBgBack 0.5s cubic-bezier(0.68,-0.55,0.29,1.65) forwards";
+
+          vps.style.color = "#fff";
       }
   });
 });
