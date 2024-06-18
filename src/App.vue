a03b348ac98ba875181e4e915485dbe4899d6dfc
Antoine Lauzis <antoine.lauzis@etudiant.univ-reims.fr>
2024-06-18 23:05:08 +0200

    feat : first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+      <nav class="h-full flex justify-evenly items-center">
+        <RouterLink to="/">Home</RouterLink> |
+        <RouterLink to="/works">My Works</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
+  
+  <footer>
+    <p>Footer</p>
+  </footer>
+</template>
