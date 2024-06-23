d065965188afc7852e03c0914ff32711b7ca52be
Antoine Lauzis <antoine.lauzis@etudiant.univ-reims.fr>
2024-06-23 17:03:40 +0200

    feat : title works

---
+++ src/components/SVGs/wave_extended_svg.vue
@@ -0,0 +1,18 @@
+<template>
+    <svg class="wave_extended_svg" id="Calque_1" data-name="Calque 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 159.41 26.29" overflow="visible">
+        <path class="cls-1" d="M0,13.15c7.34-10.32,19.23-10.32,26.57,0s19.23,10.32,26.57,0,19.23-10.32,26.57,0,19.23,10.32,26.57,0,19.23-10.32,26.56,0,19.24,10.32,26.57,0"/>
+    </svg>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .cls-1 {
+        fill:none;
+        stroke:#4e485f;
+        stroke-miterlimit:10;
+        stroke-width:10px;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,7 @@
 <script setup>
   import star_1_svg from '../components/SVGs/star_1_svg.vue'
   import arrow_down_svg from '../components/SVGs/arrow_down_svg.vue'
-  // import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
-
-  // lottie.loadAnimation({
-  //   container: document.getElementById('animation'),
-  //   renderer: 'svg',
-  //   loop: true,
-  //   autoplay: true,
-  //   path: '../../public/res/videos/home_blur.json' // Remplace par le chemin vers ton fichier JSON exporté
-  // });
+  import wave_extended_svg from '../components/SVGs/wave_extended_svg.vue'
 </script>
 
 <template>
@@ -46,35 +38,52 @@
     </div>
     <div id="competences">
       <div class="container grid grid-cols-12">
+
         <div class="text col-span-5">
+
           <h2 class="h2">
             Votre site <span>w</span>eb <br>
             sur mes<span>u</span>re
           </h2>
+
           <p>
             Site e-commerce, vitrine en ligne, blog personnel ou encore bien plus encor. Je m'adapte à vos besoins et vos envies pour faire naître votre vision ! Et si vous n'avez pas d'idée encore précises je vous aide à préparer et structurer le projet qui vous correspond.
             <br><br>
             Mes compétences permettent de collaborer ensemble pour que votre produit soit le plus personnalisé possible. La certification Opquast atteste de mon engagement à respecter les meilleures pratiques web et à fournir des sites web de qualité, accessibles et performants.
           </p>
+
         </div>
+
         <div class="illustration col-span-7">
           <img src="/res/images/illustration_competences.png" alt="">
         </div>
+
       </div>
     </div>
     <div id="realisations">
-      <div class="container grid grid-cols-12">
-        <div class="text col-span-6">
-          <h2 class="h2">
-            Mes <span>r</span>éalisations
-          </h2>
-          <p>
-            Découvrez mes dernières réalisations et projets en cours. Chaque projet est unique et réalisé sur mesure pour répondre aux besoins de mes clients. Vous pouvez consulter les détails de chaque projet et voir les technologies utilisées.
-          </p>
+
+      <div class="title-container container">
+
+        <div class="waves">
+          <wave_extended_svg />
+          <wave_extended_svg />
         </div>
-        <div class="illustration col-span-6">
+
+        <h2 class="h2">
+          Quelques tra<span>v</span>aux
+        </h2>
+
+        <div class="waves">
+          <wave_extended_svg />
+          <wave_extended_svg />
         </div>
+
       </div>
+
+      <div class="real-container container">
+        
+      </div>
+      
     </div>
   </main>
 </template>
